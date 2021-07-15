5d63eef838d34867f51e10a2a993943f123ac041
Viviane Bresolin <vivianebresolin@gmail.com>
2021-07-14 23:40:51 -0300

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DragonCreateComponent } from './components/dragon/dragon-create/dragon-create.component';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "",
+  component: HomeComponent
+},
+{
+  path: "login",
+  component: LoginComponent
+},
+{
+  path: "dragon/create",
+  component: DragonCreateComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
